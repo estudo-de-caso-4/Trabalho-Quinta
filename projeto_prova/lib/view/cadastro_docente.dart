88c4978106ea910572c6a03122799a846382e12e
Raffael Bino <raffael_bino18@yahoo.com.br>
2020-04-20 16:42:57 -0300

    Add tela cadastro completa

--- projeto_prova/lib/view/cadastro_docente.dart
+++ projeto_prova/lib/view/cadastro_docente.dart
@@ -9,10 +9,96 @@ class CadastroDocente extends StatefulWidget {
 }
 
 class _CadastroDocenteState extends State<CadastroDocente> {
+  final _formKey = GlobalKey<FormState>();
+  final _textController = TextEditingController();
+  final _textController1 = TextEditingController();
+  final _textController2 = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container(
-       
-    );
+   return Scaffold(
+        appBar: AppBar(
+          title: Text("Cadastro"),
+        ),
+        body: Builder(builder: (BuildContext context) {
+          return Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      labelText: "Nome Completo",
+                      suffixIcon: Icon(Icons.person),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10)
+                        ),
+                      )
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Entre com o nome";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: _textController1,
+                    decoration: InputDecoration(
+                      labelText: "Sua Matrícula",
+                      suffixIcon: Icon(Icons.dvr),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10)
+                        ),
+                      )
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Entre com a sua matrícula";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TextFormField(
+                    controller: _textController2,
+                    decoration: InputDecoration(
+                      labelText: "Crie sua Senha",
+                      suffixIcon: Icon(Icons.lock),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10)
+                        ),
+                      )
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return "Esse espaço não pode ficar vazio";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                RaisedButton(
+                    child: Text("Cadastrar"),
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        Scaffold.of(context).showSnackBar(SnackBar(
+                          content: Text('Conta Criada'),
+                        ));
+                      }
+                    })
+              ],
+            ),
+          );
+        }));
   }
 }
